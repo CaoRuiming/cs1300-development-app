1972d2d4f36bde8573cb61ef3c8e0159c447b9ee
CaoRuiming <rcao22556@gmail.com>
2020-11-28 16:41:01 -0500

    cleaned up code in Gallery.js

--- src/Gallery.js
+++ src/Gallery.js
@@ -14,13 +14,17 @@ export default function Gallery({
     const disabled = disabledItems.some(x => x.id === id);
     return (
       <Card key={id} className={className}>
-        {!shoppingCart ? <Card.Img variant="top" src={image} /> : null}
+        {shoppingCart ? null : <Card.Img variant="top" src={image} />}
         <Card.Body>
           <Card.Title>{`${courseCode}: ${name}`}</Card.Title>
           <Card.Text>{`Weekly Hours: ${weeklyHours}`}</Card.Text>
           <Card.Text>{`Semester Offered: ${semester}`}</Card.Text>
           <Card.Text>{`Concentration Track: ${track}`}</Card.Text>
-          {!shoppingCart ? (
+          {shoppingCart ? (
+            <Button variant="danger" onClick={() => handleButtonClicked(id)}>
+              Remove
+            </Button>
+          ) : (
             <Button
               variant="primary"
               disabled={disabled}
@@ -28,40 +32,36 @@ export default function Gallery({
             >
               {disabled ? 'Added' : 'Add'}
             </Button>
-          ) : (
-            <Button variant="danger" onClick={() => handleButtonClicked(id)}>
-              Remove
-            </Button>
           )}
         </Card.Body>
       </Card>
     );
   });
 
-  // if course gallery
-  if (!shoppingCart) {
+  if (shoppingCart) {
+    // if cart gallery, we need to calculate the total weekly hours
+    const weeklyHours = items.map(x => x.weeklyHours);
+    const totalWeeklyHours = weeklyHours.reduce((a, b) => a + b, 0);
+    const formatted = `${totalWeeklyHours}`.length > 5
+      ? `${totalWeeklyHours}`.substring(0, 5)
+      : `${totalWeeklyHours}`;
     return (
-      <Card id="course-gallery">
+      <Card id="cart-gallery">
+        <h2>Shopping Cart</h2>
         <div className="gallery">
           {items ? itemCards : null}
         </div>
+        <h4>Total Weekly Hours: {formatted}</h4>
       </Card>
     );
   }
-  
-  // if cart gallery, we need to calculate the total weekly hours
-  const weeklyHours = items.map(x => x.weeklyHours);
-  const totalWeeklyHours = weeklyHours.reduce((a, b) => a + b, 0);
-  const formatted = `${totalWeeklyHours}`.length > 5
-    ? `${totalWeeklyHours}`.substring(0, 5)
-    : `${totalWeeklyHours}`;
+
+  // else we are displaying the course gallery
   return (
-    <Card id="cart-gallery">
-      <h2>Shopping Cart</h2>
+    <Card id="course-gallery">
       <div className="gallery">
         {items ? itemCards : null}
       </div>
-      <h4>Total Weekly Hours: {formatted}</h4>
     </Card>
   );
 }
